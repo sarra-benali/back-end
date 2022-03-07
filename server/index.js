024dd6f7172205e972af73472ccefcc753e00b6f
sarra-benali <sarraba4@gmail.com>
2022-03-07 10:29:58 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const logger = require("morgan");
 var product = require("./database_mysql");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+if(process.env.NODE_ENV ==="production" ){
+  app.use(express.static('build'));
+  app.get('*',(req, res)=>{
+    req.sendFile(path.resolve(__dirname, 'build','index.html'));
+  })
+}
 app.use(express.json());
 app.use(cors());
 app.use(logger("dev"));
